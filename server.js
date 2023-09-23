9a88a9906adc2a21ce9282ba664805813526bbfb
mahat sumit <mahatsumit5@gmail.com>
2023-09-23 19:08:11 +1000

    build folder chnaged

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 import dotenv from "dotenv";
 dotenv.config(); //using dotenv to process dotenv key
